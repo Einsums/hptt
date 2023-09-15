2532c71f5b0861935cad4396d4c10a544d8b23c9
Justin Turney <justin.turney@gmail.com>
2023-09-15 13:21:58 -0400

    Moving headers around.

--- src/plan.cpp
+++ src/plan.cpp
@@ -1,28 +1,34 @@
 
 
-#include <utils.h>
-#include <plan.h>
-#include <compute_node.h>
+#include <hptt/compute_node.h>
+#include <hptt/plan.h>
+#include <hptt/utils.h>
 
 namespace hptt {
 
-   Plan::Plan(std::vector<int>loopOrder, std::vector<int>numThreadsAtLoop) : rootNodes_(nullptr), loopOrder_(loopOrder), numThreadsAtLoop_(numThreadsAtLoop) {
-      numTasks_ = 1;
-      for(auto nt : numThreadsAtLoop)
-         numTasks_ *= nt;
-      rootNodes_ = new ComputeNode[numTasks_];
-   }
-
-   Plan::~Plan() {
-      if ( rootNodes_ != nullptr )
-         delete[] rootNodes_;
-   }
-
-   const ComputeNode* Plan::getRootNode_const(int threadId) const { return &rootNodes_[threadId]; }
-   ComputeNode* Plan::getRootNode(int threadId) const { return &rootNodes_[threadId]; }
-
-   void Plan::print() const {
-      printVector(loopOrder_,"LoopOrder");
-      printVector(numThreadsAtLoop_,"Parallelization");
-   }
+Plan::Plan(std::vector<int> loopOrder, std::vector<int> numThreadsAtLoop)
+    : rootNodes_(nullptr), loopOrder_(loopOrder),
+      numThreadsAtLoop_(numThreadsAtLoop) {
+  numTasks_ = 1;
+  for (auto nt : numThreadsAtLoop)
+    numTasks_ *= nt;
+  rootNodes_ = new ComputeNode[numTasks_];
 }
+
+Plan::~Plan() {
+  if (rootNodes_ != nullptr)
+    delete[] rootNodes_;
+}
+
+const ComputeNode *Plan::getRootNode_const(int threadId) const {
+  return &rootNodes_[threadId];
+}
+ComputeNode *Plan::getRootNode(int threadId) const {
+  return &rootNodes_[threadId];
+}
+
+void Plan::print() const {
+  printVector(loopOrder_, "LoopOrder");
+  printVector(numThreadsAtLoop_, "Parallelization");
+}
+} // namespace hptt
